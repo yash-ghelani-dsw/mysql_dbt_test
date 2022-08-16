14ec2b0f5b01792e69945bd9d225a8bed5323e3b
yash-ghelani-dsw <89910877+yash-ghelani-dsw@users.noreply.github.com>
2022-08-16 13:28:19 +0530

    Add files via upload

---
+++ target/run/main_data_dbt/models/energy.sql
@@ -0,0 +1,15 @@
+
+
+  create  table "ETL_logical"."mysql_large_data"."energy__dbt_tmp"
+  as (
+    with data_sql as (
+    select * from mysql_large_data.large_data
+),
+
+final as (
+    select row_id, energy_count, row_id * 100 as row_id_100
+    from data_sql
+)
+
+select * from final
+  );
